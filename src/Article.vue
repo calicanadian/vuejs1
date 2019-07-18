043e87560480d828f100afc8be18dea1eec52de5
calicanadian <ryan.stuhl@gmail.com>
2019-07-18 10:19:55 -0700

    Added Article component and Author component. Encountered a bug here.

---
+++ src/Article.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="article">
+    <h1>{{ title }}</h1>
+
+    <p>Published on {{ published | moment }}</p>
+    <p class="lead">{{ content }}</p>
+    <app-author></app-author>
+  </div>
+</template>
+
+<script>
+  // import moment from 'moment';
+  import Author from './Author.vue';
+
+  export default {
+    data() {
+      return {
+        title: "Vue.js is awesome and powerful",
+        published: new Date(),
+        content: "Vue.js' power is only limited by its awesomeness."
+      };
+    },
+    filters: {
+      moment: function(value) {
+        return moment(value).format('MMMM Do');
+      }
+    },
+    components: {
+      'app-author': Author
+    }
+  }
+</script>
